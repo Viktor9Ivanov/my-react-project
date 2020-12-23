81e4861e1762da4830402abef62d537e828003c3
viktor-bit <veltex@veltex.lt>
2020-12-23 10:25:24 +0200

    header repair

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import logo from './logo.svg'
 import classes from './Header.module.css'
+
 const Header = () => {
     return (
-        // <div>
         <header className={classes.header}>
             <img src={logo} alt="logo" />
-            {/* <img src = 'https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/026/full/react.png' alt='Heder images' /> */}
-            <a className={classes}>  My react project</a>
+            <span />
         </header>
-        // </div>
-
     )
 }
+
 export default Header
